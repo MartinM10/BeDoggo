98a2868857f701cef1650ebe407c713f86e9262b
Martín Salvachúa <martin.salvachua1@gmail.com>
2025-01-31 08:47:52 +0100

    Staticfiles set in setting.py to serve static files

--- BeDoggo/settings.py
+++ BeDoggo/settings.py
@@ -176,11 +176,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+"""
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     # "/var/www/static/",
 ]
+"""
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
