c3dcb18b1af15bf50836528574439349cf4f1f80
Martín Salvachúa <martin.salvachua1@gmail.com>
2025-02-06 12:39:10 +0100

    Added corseheaders

--- BeDoggo/settings.py
+++ BeDoggo/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
+    "corsheaders",
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_spectacular',
@@ -102,12 +103,20 @@ LOGOUT_REDIRECT_URL = '/accounts/login/'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8081",  # Permite las peticiones desde React Native
+    "http://127.0.0.1:8081",
+]
+
+CORS_ALLOW_CREDENTIALS = True  # Permite enviar cookies (si usas autenticación con sesión)
 
 ROOT_URLCONF = 'BeDoggo.urls'
 
